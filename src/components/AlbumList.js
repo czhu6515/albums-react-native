b86859e2a15271ca9966091e828471b6a33c6a15
Chen <chenyu.zhu86@gmail.com>
2018-12-01 19:41:06 -0500

    fetched data from albums api when AlbumList component mounts

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,13 +2,29 @@ import React, {Component} from 'react';
 import {Text, View } from 'react-native';
 
 export default class AlbumList extends Component{
-  
-  render(){
+  state = {
+    albums: []
+  }
 
+  componentWillMount(){
+    return fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      albums = responseJson
+      this.setState({ albums })
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  render(){
     return(
       <View>
+        {console.log(this.state.albums)}
         <Text>Hello!</Text>
       </View>
-      )
+    )
+
   }
 }
