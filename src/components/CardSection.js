b3dc653192c78efcb1d00b5f282c8dca7048f5a5
Chen <chenyu.zhu86@gmail.com>
2018-12-01 20:03:36 -0500

    created Card and CardSection components and styled Card component

---
+++ src/components/CardSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const CardSection = () => {
+  return (
+    <View>
+      <Text>Awesome</Text>
+    </View>
+  )
+}
+
+export default CardSection
