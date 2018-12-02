b451bac9ef8fb6e46d399ff83c030b82e2803b06
Chen <chenyu.zhu86@gmail.com>
2018-12-01 19:05:50 -0500

    created AlbumList component and imported into App

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header'
+import AlbumList from './src/components/AlbumList'
 
 
 export default class App extends React.Component {
@@ -10,7 +11,10 @@ export default class App extends React.Component {
   
   render() {
     return (
-      <Header headerText={this.state.headerText}/>
+      <View>
+        <Header headerText={this.state.headerText}/>
+        <AlbumList />
+      </View>
     )     
   }
 }
